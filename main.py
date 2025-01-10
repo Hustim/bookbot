b63eff9b829a0ed14da5d73ebb235fe581282309
Hustim <tomasgruber04@gmail.com>
2025-01-10 23:41:27 +0100

    added letter-counting function

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 def main():
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+        file_contents = f.read() 
     words_list = count_words(file_contents)
     word_count = len(words_list)
-    print(word_count)
+    letter_count = count_char(file_contents)
     
 def count_words(string):
     words = string.split()
     return words
 
+def count_char(string):
+    char_dict = create_letter_dict()
+    string = string.lower()
+    for letter in string:
+        if 96 < ord(letter) < 123:
+            char_dict[letter] += 1
+    return char_dict
 
-
+def create_letter_dict():
+    char_dict = {}
+    for i in range(97, 123):
+        char_dict[chr(i)] = 0
+    return char_dict
 
 main()
